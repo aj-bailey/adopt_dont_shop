89dfa7c1f0adf2e2f1d737aa89f4db519c70fa95
Adam Bailey <ab67319@gmail.com>
2023-02-14 15:21:48 -0700

    feat: create #pets_approved?

--- app/models/application.rb
+++ app/models/application.rb
@@ -10,4 +10,8 @@ class Application < ApplicationRecord
   validates :description, presence: true
 
   enum status: ["In Progress", "Pending", "Accepted", "Rejected"]
+
+  def pets_approved?
+    self.pet_applications.where.not(status: "Approved").count == 0
+  end
 end
