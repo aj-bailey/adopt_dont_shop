e613a3f83ebb6e4c11602b18f2418a5bb1add7e2
Brian Hayes <Brian.hayes1200@gmail.com>
2023-02-15 14:57:40 -0700

    Test: add test for seeing average pet age on a admin shelter show page

--- spec/features/admin/shelters/show_spec.rb
+++ spec/features/admin/shelters/show_spec.rb
@@ -3,13 +3,23 @@ require 'rails_helper'
 RSpec.describe 'Admin Shelters Show page' do 
   describe 'as a visitor' do
     describe 'when I visit an admin shelter show page' do
-      it 'will show the full name and address of that shelter' do 
-      @shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+      before(:each) do 
+        @shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+        @pet_1 = @shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: false)
+        @pet_2 = @shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
+        @pet_4 = @shelter_1.pets.create(name: 'Ann', breed: 'ragdoll', age: 5, adoptable: true)
+        visit "/admin/shelters/#{@shelter_1.id}"
+      end 
 
-      visit "/admin/shelters/#{@shelter_1.id}"
+      it 'will show the full name and address of that shelter' do 
+        expect(page).to have_content(@shelter_1.name)
+        expect(page).to have_content(@shelter_1.city)
+      end
       
-      expect(page).to have_content(@shelter_1.name)
-      expect(page).to have_content(@shelter_1.city)
+      it 'will show the average pet age for shelter in a section called statistics' do 
+        within(".statistics") {
+          expect(page).to have_content("Average Pet Age: 4.3")
+        }
       end
     end
   end
