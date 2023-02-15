935761ad1754d2489b57d14ed25fb290b525aa5e
Adam Bailey <ab67319@gmail.com>
2023-02-15 15:54:36 -0700

    test: create feature test for showing count of adopted pets on admin shelter show page

--- spec/features/admin/shelters/show_spec.rb
+++ spec/features/admin/shelters/show_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'Admin Shelters Show page' do
       before(:each) do 
         @shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
         @pet_1 = @shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: false)
-        @pet_2 = @shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
+        @pet_2 = @shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: false)
         @pet_4 = @shelter_1.pets.create(name: 'Ann', breed: 'ragdoll', age: 5, adoptable: true)
         visit "/admin/shelters/#{@shelter_1.id}"
       end 
@@ -19,6 +19,18 @@ RSpec.describe 'Admin Shelters Show page' do
       it 'will show the average pet age for shelter in a section called statistics' do 
         within(".statistics") {
           expect(page).to have_content("Average Pet Age: 4.3")
+        }
+      end
+
+      it 'will show count of pets that have been adopted in a section called statistics' do
+        application = Application.create!(name: "Brian", street_address: "853 West Linden st", city: "Louisville", state: "colorado", zip_code: "80027", description: "I like animals", status: 2)
+        application.pet_applications.create!(pet: @pet_1, status: 1)
+        application.pet_applications.create!(pet: @pet_2, status: 1)
+
+        visit "/admin/shelters/#{@shelter_1.id}"
+
+        within(".statistics") {
+          expect(page).to have_content("Count of Adopted Pets: 2")
         }
       end
     end
