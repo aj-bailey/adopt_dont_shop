17a1aa21edf365755260030b77b1b2cd1beed926
Brian Hayes <Brian.hayes1200@gmail.com>
2023-02-14 17:59:13 -0700

    Feat: add functionality to display a shelters name and address

--- app/controllers/admin_shelters_controller.rb
+++ app/controllers/admin_shelters_controller.rb
@@ -3,4 +3,8 @@ class AdminSheltersController < ApplicationController
     @shelters = Shelter.order_by_reverse_alphabetical_names
     @shelters_with_pending_applications = Shelter.pending_applications
   end
+
+  def show
+    @shelter_name_and_address = Shelter.name_and_address(params[:id])
+  end
 end
